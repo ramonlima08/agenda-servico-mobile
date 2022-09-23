06e04c79c0a2216a7bf3eee98a3a3e8b96ed813a
Ramon Lima <ramonlima08@gmail.com>
2022-09-23 14:56:40 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'agenda_cel',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
